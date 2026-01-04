22dae27d5944b5524473f3f71b70de82b8162f89
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-04 17:53:40 +0530

    feat: implement login route with JWT authentication and password validation

--- apps/server/src/routes/auth.ts
+++ apps/server/src/routes/auth.ts
@@ -3,6 +3,8 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { env } from "@100x-sem-1-assignment/env/server";
+import jwt from "jsonwebtoken";
 
 export const authRouter = new Hono();
 
@@ -55,5 +57,47 @@ authRouter.post(
     }
 
     return c.json({ success: true, data: user }, 201);
+  }
+);
+
+authRouter.post(
+  "/login",
+  zValidator("json", loginSchema, (result, c) => {
+    if (!result.success) {
+      return c.json({ success: false, error: "Invalid request schema" }, 400);
+    }
+  }),
+  async (c) => {
+    const { email, password } = c.req.valid("json");
+
+    const user = await db.query.users.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return c.json(
+        { success: false, error: "Invalid email or password" },
+        400
+      );
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return c.json(
+        { success: false, error: "Invalid email or password" },
+        400
+      );
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return c.json({ success: true, data: { token } }, 200);
   }
 );
